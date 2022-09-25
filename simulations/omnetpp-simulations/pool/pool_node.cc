b64a3a8ff10784fae878e4e8b6369db01923b82b
pool2win <pool2win@users.noreply.github.com>
2022-09-25 23:02:17 +0200

    Reorganise pool simulation source

--- simulations/omnetpp-simulations/pool/pool_node.cc
+++ simulations/omnetpp-simulations/pool/pool_node.cc
@@ -1,12 +1,6 @@
 //
-    // This file is part of an OMNeT++/OMNEST simulation example.
-    //
-    // Copyright (C) 2003 Ahmet Sekercioglu
-    // Copyright (C) 2003-2015 Andras Varga
-    //
-    // This file is distributed WITHOUT ANY WARRANTY. See the file
-    // `license' for details on this and other legal matters.
-    //
+// This file builds on the tictoc example from the omnetpp source code.
+//
 
     #include <stdio.h>
     #include <string.h>
@@ -15,13 +9,9 @@
     using namespace omnetpp;
 
     /**
-     * Let's make it more interesting by using several (n) `tic' modules,
-     * and connecting every module to every other. For now, let's keep it
-     * simple what they do: module 0 generates a message, and the others
-     * keep tossing it around in random directions until it arrives at
-     * module 3.
+     * Node in the simulation
      */
-    class Txc1 : public cSimpleModule
+    class PoolNode : public cSimpleModule
     {
       protected:
         double forwarding_probability;
@@ -33,32 +23,32 @@
         virtual void handleMessage(cMessage *msg) override;
     };
 
-    Define_Module(Txc1);
+    Define_Module(PoolNode);
 
-    void Txc1::initialize()
+    void PoolNode::initialize()
     {
         forwarding_probability = par("forwarding_probability");
         initial_delay_multiplier = par("initial_delay_multiplier");
         // Boot the process scheduling the initial message as a self-message.
         char msgname[20];
-        sprintf(msgname, "tic-%d", getIndex());
+        sprintf(msgname, "share-%d", getIndex());
         cMessage *msg = new cMessage(msgname);
         scheduleAt(initial_delay_multiplier * getIndex(), msg);
     }
 
-    void Txc1::handleMessage(cMessage *msg)
+    void PoolNode::handleMessage(cMessage *msg)
     {
         forwardMessage(msg);
     }
 
-    void Txc1::sendCopyOf(cMessage *msg, int index)
+    void PoolNode::sendCopyOf(cMessage *msg, int index)
     {
         // Duplicate message and send the copy.
         cMessage *copy = (cMessage *)msg->dup();
         send(copy, "gate$o", index);
     }
 
-    bool Txc1::shouldForward(cMessage *msg, int index)
+    bool PoolNode::shouldForward(cMessage *msg, int index)
     {
         if (msg->isSelfMessage())
                 return true;
@@ -68,10 +58,9 @@
                 (roll <= forwarding_probability));
     }
 
-    void Txc1::forwardMessage(cMessage *msg)
+    void PoolNode::forwardMessage(cMessage *msg)
     {
         // loop over all gates and send message out on each.
-        // TODO - add probabilistic forwarding
         auto num_gates = gateSize("gate$o") - 1;
         for (auto index = 0; index <= num_gates; index++){
             if (shouldForward(msg, index)) {
