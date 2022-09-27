1acebeb68d5ab9188e2227880c406470871987c5
pool2win <pool2win@users.noreply.github.com>
2022-09-27 09:48:02 +0200

    Add hop count, and limit forwarding

--- simulations/omnetpp-simulations/pool/pool_node.cc
+++ simulations/omnetpp-simulations/pool/pool_node.cc
@@ -17,19 +17,18 @@
       protected:
         double forwarding_probability;
         int initial_delay_multiplier;
-        virtual void sendCopyOf(cMessage *msg, int index);
-        virtual void forwardMessage(cMessage *msg);
-        virtual bool shouldForward(cMessage *msg, int index);
+        int max_num_hops;
+        virtual void sendCopyOf(PoolMessage *msg, int index);
+        virtual void processMessage(PoolMessage *msg);
+        virtual bool shouldForward(PoolMessage *msg, int index);
         virtual void initialize() override;
         virtual void handleMessage(cMessage *msg) override;
+        virtual void sendFirstMessage();
     };
 
     Define_Module(PoolNode);
 
-    void PoolNode::initialize()
-    {
-        forwarding_probability = par("forwarding_probability");
-        initial_delay_multiplier = par("initial_delay_multiplier");
+    void PoolNode::sendFirstMessage(){
         // Boot the process scheduling the initial message as a self-message.
         char msgname[20];
         sprintf(msgname, "share-%d", getIndex());
@@ -37,37 +36,52 @@
         scheduleAt(initial_delay_multiplier * getIndex(), msg);
     }
 
-    void PoolNode::handleMessage(cMessage *msg)
+    void PoolNode::initialize()
     {
-        forwardMessage(msg);
+        forwarding_probability = par("forwarding_probability");
+        initial_delay_multiplier = par("initial_delay_multiplier");
+        max_num_hops = par("max_num_hops");
+        EV<< "max num hops " << max_num_hops << "\n";
+        if (getIndex() == 0) {
+            sendFirstMessage();
+        }
     }
 
-    void PoolNode::sendCopyOf(cMessage *msg, int index)
+    void PoolNode::handleMessage(cMessage *msg)
+    {
+        PoolMessage* pool_msg = check_and_cast<PoolMessage* >(msg);
+        EV << "Message " << pool_msg << " arrived after " << pool_msg->getHopCount() << " hops.\n";
+        processMessage(pool_msg);
+    }
+
+    void PoolNode::sendCopyOf(PoolMessage *msg, int index)
     {
         // Duplicate message and send the copy.
-        cMessage *copy = (cMessage *)msg->dup();
+        PoolMessage *copy = msg->dup();
+        copy->setHopCount(copy->getHopCount() + 1);
         send(copy, "gate$o", index);
     }
 
-    bool PoolNode::shouldForward(cMessage *msg, int index)
+    bool PoolNode::shouldForward(PoolMessage *pool_msg, int index)
     {
-        PoolMessage *pool_msg = check_and_cast<PoolMessage *>(msg);
         if (pool_msg->isSelfMessage())
                 return true;
         auto received_from = pool_msg->getArrivalGate();
         double roll = uniform(0, 1);
+        auto hop_count = pool_msg->getHopCount();
         return (gate("gate$i", index) != received_from &&
+                hop_count <= max_num_hops - 1 &&
                 (roll <= forwarding_probability));
     }
 
-    void PoolNode::forwardMessage(cMessage *msg)
+    void PoolNode::processMessage(PoolMessage *pool_msg)
     {
         // loop over all gates and send message out on each.
         auto num_gates = gateSize("gate$o") - 1;
         for (auto index = 0; index <= num_gates; index++){
-            if (shouldForward(msg, index)) {
-                EV << "Forwarding message " << msg << " on port channel" << index << "\n";
-                sendCopyOf(msg, index);
+            if (shouldForward(pool_msg, index)) {
+                EV << "Forwarding message " << pool_msg << " on port channel" << index << "\n";
+                sendCopyOf(pool_msg, index);
             }
         }
     }
